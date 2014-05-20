eadcfb6a0b4652ddfa7453501c26d2b00aa3a4c0
Rodrigo Duarte Louro <digao.louro@gmail.com>
2014-05-19 23:38:21 -0300

    Inserindo Main

---
+++ src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Aqui temos que pedir o package do cara? Rodar os testes JUNIT? "
+				+ "ou outra opção?");
+	}
+}
