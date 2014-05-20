440590cb9491fbaa09da17926dfa45f86c4c6025
Rodrigo Duarte Louro <digao.louro@gmail.com>
2014-05-19 23:15:17 -0300

    Leitura do Arquivo XML feita.
    Dado um Arquivo XML criado pelo software desenvolvido na
    Atividade 4 é criado um objeto do tipo TodasMCDC semelhante ao
    criado na Atividade4

---
+++ src/parser/LeituraXML.java
@@ -0,0 +1,38 @@
+package parser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Vector;
+
+import modelo.Classe;
+import modelo.Decisao;
+import modelo.TodasMCDC;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+
+public class LeituraXML {
+	
+
+	/** Cria um objeto TodasMCDC a partir de um arquivo XML
+	 * @param path Caminho do arquivo XML
+	 * @return objeto TodasMCDC com os requisitos de teste para a cobertura do MCDC
+	 */
+	
+	public TodasMCDC getRequisitosMCDC (String path) throws IOException {
+
+		XStream xStream = new XStream(new DomDriver());
+		File file = new File(path);
+		FileInputStream xmlFile = new FileInputStream(file);
+ 
+		//Alias feitos na geração do arquivo precisam ser feitos agora também. 
+		xStream.alias("TodasMCDC", TodasMCDC.class);		
+		xStream.alias("decision", Decisao.class);
+		
+		TodasMCDC todasMCDC = (TodasMCDC) xStream.fromXML(xmlFile); 
+		
+		return todasMCDC; 
+	}
+}
---
+++ testes/parser/LeituraXMLTeste.java
@@ -0,0 +1,38 @@
+package parser;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import modelo.Classe;
+import modelo.Metodo;
+import modelo.TodasMCDC;
+
+import org.junit.Test;
+
+public class LeituraXMLTeste {
+
+	@Test
+	public void getRequisitosMCDCTeste() throws IOException {
+		//To fazendo esse teste para um arquivo especifico então acho que não está bom. 
+		
+		LeituraXML leitor = new LeituraXML(); 
+		String path = "/home/digao/MCDC.xml"; 
+		TodasMCDC todasMCDC = leitor.getRequisitosMCDC(path);
+		List<Classe> classes = todasMCDC.getClasses(); 
+		assertEquals(classes.size(), 3);
+		
+		List<Metodo> metodos1 = classes.get(0).getMetodos(); 
+		List<Metodo> metodos2 = classes.get(1).getMetodos(); 
+		List<Metodo> metodos3 = classes.get(2).getMetodos(); 
+		
+		assertEquals(metodos1.size(), 2);
+		assertEquals(metodos2.size(), 2);
+		assertEquals(metodos3.size(), 2);
+		
+		//TODO fixar um arquivo XML especifico e fazer mais testes pra ver se está funcionando. 
+		
+	}
+	
+}
